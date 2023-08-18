fc5d36970d190fef185a5b4188c7c41430403703
Shamil Cheerchiev <117973694+Shmel97@users.noreply.github.com>
2023-08-18 18:03:43 +0300

    add tests profileCard, add user event, fix virtualized

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -15,6 +15,7 @@ interface ArticleListProps {
     isLoading?: boolean;
     target?: HTMLAttributeAnchorTarget;
     view?: ArticleView;
+    virtualized?: boolean;
 }
 
 const getSkeletons = (view: ArticleView) => {
@@ -27,7 +28,7 @@ const getSkeletons = (view: ArticleView) => {
 
 export const ArticleList = memo((props: ArticleListProps) => {
     const {
-        className, view = ArticleView.SMALL, articles, isLoading, target,
+        className, view = ArticleView.SMALL, articles, isLoading, target, virtualized = true,
     } = props;
     const { t } = useTranslation();
 
@@ -90,17 +91,32 @@ export const ArticleList = memo((props: ArticleListProps) => {
                     ref={registerChild}
                     className={classNames(cls.ArticleList, {}, [className, cls[view]])}
                 >
-                    <List
-                        height={height ?? 700}
-                        rowCount={rowCount}
-                        rowHeight={isBig ? 700 : 330}
-                        rowRenderer={rowRender}
-                        width={width ? width - 80 : 700}
-                        autoHeight
-                        onScroll={onChildScroll}
-                        isScrolling={isScrolling}
-                        scrollTop={scrollTop}
-                    />
+                    {virtualized
+                    ? (
+                            <List
+                                height={height ?? 700}
+                                rowCount={rowCount}
+                                rowHeight={isBig ? 700 : 330}
+                                rowRenderer={rowRender}
+                                width={width ? width - 80 : 700}
+                                autoHeight
+                                onScroll={onChildScroll}
+                                isScrolling={isScrolling}
+                                scrollTop={scrollTop}
+                            />
+                        )
+                    : (
+                        articles.map(item => (
+                            <ArticleListItem
+                                article={item}
+                                view={view}
+                                target={target}
+                                key={item.id}
+                                className={cls.card}
+                            />
+                        ))
+                        )}
+
                     {isLoading && getSkeletons(view)}
                 </div>
             )}
--- src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
+++ src/features/editableProfileCard/ui/EditableProfileCard/EditableProfileCard.tsx
@@ -116,6 +116,7 @@ export const EditableProfileCard = memo((props: EditableProfileCardProps) => {
                         key={err}
                         theme={TextTheme.ERROR}
                         text={validateErrorTranslates[err]}
+                        data-testid={'EditableProfileCard.Error'}
                     />
                 ))}
                 <ProfileCard
